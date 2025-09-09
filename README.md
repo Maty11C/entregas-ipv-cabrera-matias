7409a433e7c9e3e0d1677257d3fac6bab49a7a0c
Matías Cabrera <mcabrera@cys.com.ar>
2025-09-09 01:44:03 -0300

    Export entrega 3

--- README.md
+++
@@ -1,6 +0,0 @@
-# Introducción al Desarrollo de Videojuegos
-
-## Entregas
-1. [Tu primer videojuego, por la comunidad de Godot!](./entrega-1-cabrera-matias)
-2. [Player, proyectiles y torretas](./entrega-2-cabrera-matias)
-3. [Físicas](./entrega-3-cabrera-matias)
--- entrega-2-cabrera-matias/main.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-func _ready() -> void:
-	$Player._set_projectile_container(self)
-	var viewport = get_viewport()
-	$Turret1.set_values(viewport, $Player, self)
-	$Turret2.set_values(viewport, $Player, self)
-	$Turret3.set_values(viewport, $Player, self)
