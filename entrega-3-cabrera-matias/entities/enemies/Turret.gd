f1972106d45c7b437bd51aba3b8e8b05136a67bc
Matías Cabrera <mcabrera@cys.com.ar>
2025-09-18 00:45:47 -0300

    Se agrega bonus. Las torretas solo disparan si no existe una pared en el medio

--- entrega-3-cabrera-matias/entities/enemies/Turret.gd
+++ entrega-3-cabrera-matias/entities/enemies/Turret.gd
@@ -2,6 +2,7 @@ extends Sprite2D
 
 @onready var fire_position: Node2D = $FirePosition
 @onready var fire_timer: Timer = $FireTimer
+@onready var ray_cast: RayCast2D = $FirePosition/RayCast2D
 
 @export var projectile_scene: PackedScene
 
@@ -17,11 +18,16 @@ func initialize(turret_pos: Vector2, projectile_container: Node) -> void:
 
 func fire_at_player() -> void:
 	var proj_instance = projectile_scene.instantiate()
-	proj_instance.initialize(
-		projectile_container,
-		fire_position.global_position,
-		fire_position.global_position.direction_to(target.global_position)
-	)
+	
+	ray_cast.target_position = target.global_position - fire_position.global_position
+	if (ray_cast.is_colliding()):
+		var collider = ray_cast.get_collider()
+		if (collider is Player):
+			proj_instance.initialize(
+				projectile_container,
+				fire_position.global_position,
+				fire_position.global_position.direction_to(target.global_position)
+			)
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	if target == null:
